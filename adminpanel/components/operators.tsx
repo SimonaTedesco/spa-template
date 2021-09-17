8fc7511ad6669a790ed41329a0ad08d1cc0b5d01
lenz <lenz@mac>
2021-09-18 00:16:59 +0200

    Awful layout

--- adminpanel/components/operators.tsx
+++ adminpanel/components/operators.tsx
@@ -1,53 +1,66 @@
 
-function Instance() {
+
+const data = [{
+    "client" : "pippo-2",
+    "cluster" : "ams",
+    "mode" : "ON",
+    "time_zone" : "GMT",
+    "max_agents" : 17,
+    "instance_sha" :  "4633506b95a4e0757d76f80",
+    "long_name" : "Pippo",
+    "reseller" : "ZEBRONS",
+    "contact_person" : "pippo",
+    "contact_email" : "me@home",
+    "billing_person" : "",
+    "billing_email" : "",
+    "expires" : "2016-11-11",
+    "instance_state" : "UNK",
+    "instance_state_up" : 0,
+    "max_retention" : 37,
+    "client_state" : "SOLD",
+    "vhostname" : "my.booboo.com",
+    "kMode" : "",
+    "kNumber" : "",
+    "kApikey" : ""
+  }]
+
+
+function Instance(prop: {n: string} ) {
     return (
         <div>
-        <b>Instance</b>
+        <div className="uk-card uk-card-default uk-card-body">
+            <b>{prop.n}</b> 
+            <a href="" className="uk-icon-button" uk-icon="link"></a>
+            <a href="" className="uk-icon-button" uk-icon="settings"></a>
+            
+            <br/>
+            Agents: 37 
+            Expires: 2021-03-23
+            State: ON
+            Sold: <span className="uk-label">Default</span>
+
+
+
+            
         </div>
+    </div>
+    
                 
     )
 }
 
-function Operator() {
+function Operator(prop: {name: string, cluster: string} ) {
     return (
         <li>
-            <a className="uk-accordion-title" href="#">Item 1</a>
+            <a className="uk-accordion-title" href="#">
+                {prop.name} ({prop.cluster})</a>
             <div className="uk-accordion-content">
 
-            
+            <div className="uk-child-width-expand@s uk-text-center" uk-grid>
+                <Instance n="gino" />
+                <Instance n="pino" />
 
-
-                <Instance />
-                <Instance />
-                
-                <table className="uk-table uk-table-divider"><thead><tr>
-<th align="left">Option</th>
-<th align="left">Value</th>
-<th align="left">Default</th>
-<th align="left">Description</th>
-</tr>
-</thead><tbody><tr>
-<td align="left"><code>active</code></td>
-<td align="left">Number</td>
-<td align="left"><code>false</code></td>
-<td align="left">Index of the element to open initially.</td>
-</tr>
-</tbody>
-</table>
-
-
-<div className="uk-child-width-expand@s uk-text-center" uk-grid>
-    <div>
-        <div className="uk-card uk-card-default uk-card-body">Item</div>
-    </div>
-    <div>
-        <div className="uk-card uk-card-default uk-card-body">Item</div>
-    </div>
-    <div>
-        <div className="uk-card uk-card-default uk-card-body">Item</div>
-    </div>
-</div>
-
+            </div>
 
             </div>
         </li>
@@ -63,8 +76,8 @@ function Operators() {
         Operators
         </h1>
         <ul uk-accordion="collapsible: false">
-        <Operator />
-        <Operator />
+        <Operator name="ZEBRON" cluster="ams"/>
+        <Operator name="CICCIA" cluster="za"/>
         
 
     </ul>
