61ee7aa77ef26e0747457bea2331ba402dc67285
SimonaTedesco <91289476+SimonaTedesco@users.noreply.github.com>
2021-10-11 22:49:17 +0200

    New Graphics and Theme Support

--- adminpanel/components/operators.tsx
+++ adminpanel/components/operators.tsx
@@ -9,14 +9,21 @@ import { addArticle } from "../store/actionCreators";
 function Instance(prop: { instance: IInstance }) {
   const i = prop.instance;
   return (
-    <div>
-      <div className="uk-card uk-card-default uk-card-body">
+    <div className="Instance uk-flex-inline">
+      <div className="uk-card uk-card-default uk-card-body cardOverwrite" uk-grid>
         <b>{i.client}</b>
-        <a href="" className="uk-icon-button" uk-icon="link"></a>
-        <a href="" className="uk-icon-button" uk-icon="settings"></a>
+        <ul class="uk-iconnav iconTopOverwrite">
+          <li><a href="" uk-icon="icon: trash"></a></li>
+        </ul>
+        <br />
+        <a href="" className="uk-icon-button iconOverwrite" uk-icon="link"></a>
+        <a href="" className="uk-icon-button iconOverwrite" uk-icon="settings"></a>
+        <a href="" className="uk-icon-button iconOverwrite" uk-icon="tag"></a>
         <br />
         Agents: {i.max_agents} <br />
-        Expires: 2021-03-23 State: ON Sold:{" "}
+        Expires: 2021-03-23 <br /> State:{" "}
+        <span className="uk-label uk-label-success">ON</span><br />
+        Sold:{" "}
         <span className="uk-label">Default</span>
       </div>
     </div>
@@ -30,9 +37,9 @@ function Operator(prop: { op: Operator }) {
   const saveInstance = React.useCallback(
     (article: IInstance) => {
       const newInst = {
-          ...article,
-          reseller: token
-      }  
+        ...article,
+        reseller: token,
+      };
       dispatch(addArticle(newInst));
     },
     [dispatch]
@@ -40,11 +47,11 @@ function Operator(prop: { op: Operator }) {
 
   return (
     <li>
-      <a className="uk-accordion-title" href="#">
+      <a className="uk-accordion-title operatorInstance" href="#">
         {prop.op.name} ({prop.op.token})
       </a>
       <div className="uk-accordion-content">
-        <div className="uk-child-width-expand@s uk-text-center uk-grid">
+        <div className="uk-child-width-expand@s uk-child-width-1-4@s uk-grid-match" uk-grid>
           {prop.op.instances.map((i: IInstance) => (
             <Instance instance={i} key={i.client} />
           ))}
@@ -58,8 +65,8 @@ function Operator(prop: { op: Operator }) {
 function Operators(prop: { ops: any[] }) {
   const ops = prop.ops;
   return (
-    <div>
-      <ul uk-accordion="collapsible: false">
+    <div className="Operators">
+      <ul uk-accordion="collapsible: true">
         {ops.map((i: Operator) => (
           <Operator op={i} key={i.name} />
         ))}
